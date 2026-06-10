c3bcc4b02749267ed1fa48b0ac6cfc0f666ce673
Thunny Maridenn <thunnymaridenn@gmail.com>
2026-06-10 14:35:39 +0700

    init of week5 exercise 2

--- submissions/lib/w5/practice-stateful/ex_2_step_1_start.dart
+++ submissions/lib/w5/practice-stateful/ex_2_step_1_start.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: const Text("Favorite cards"),
-        ),
-        body: Column(
-          children: [
-            Container(
-                // YOUR CODE
-                )
-          ],
-        ),
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text("Favorite cards"),
       ),
-    ));
+      body: Column(
+        children: [
+            // YOUR CODE
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              border: Border(bottom: BorderSide(width: 1, color: Colors.grey)),
+            ),
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.fromLTRB(10,0,10,0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("title", style: TextStyle(color: Colors.blue)),
+                      SizedBox(height: 10,),
+                      Text("description")
+                    ],
+                  ),
+                ),
+                IconButton(onPressed: () => {}, icon: Icon(Icons.favorite, color: Colors.pink,))
+              ],
+            ),
+          ),
+        ],
+      ),
+    ),
+  ),
+);
