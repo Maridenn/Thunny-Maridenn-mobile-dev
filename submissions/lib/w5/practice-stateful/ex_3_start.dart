b2cf00c9b5dfcd767514f470621ee7db7f704054
Thunny Maridenn <thunnymaridenn@gmail.com>
2026-06-10 15:04:42 +0700

    init of week5 exercise 3

--- submissions/lib/w5/practice-stateful/ex_3_start.dart
+++ submissions/lib/w5/practice-stateful/ex_3_start.dart
@@ -1,36 +1,80 @@
 import 'package:flutter/material.dart';
 
 List<String> images = [
-  "assets/w4-s2/bird.jpg",
-  "assets/w4-s2/bird2.jpg",
-  "assets/w4-s2/insect.jpg",
-  "assets/w4-s2/girl.jpg",
-  "assets/w4-s2/man.jpg",
+  "assets/w5-stateful/bird.jpg",
+  "assets/w5-stateful/bird2.jpg",
+  "assets/w5-stateful/insect.jpg",
+  "assets/w5-stateful/girl.jpg",
+  "assets/w5-stateful/man.jpg",
 ];
 
-void main() => runApp(MaterialApp(
-      debugShowCheckedModeBanner: false, // Why this line ? Can you explain it ?
-      home: Scaffold(
-        backgroundColor: Colors.green[50],
-        appBar: AppBar(
-          backgroundColor: Colors.green[400],
-          title: const Text('Image viewer'),
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.navigate_before),
-              tooltip: 'Go to the previous image',
-              onPressed: () => {},
+class ImageTurner extends StatefulWidget {
+  const ImageTurner({super.key});
+
+  @override
+  State<ImageTurner> createState() => _ImageTurnerState();
+}
+
+class _ImageTurnerState extends State<ImageTurner> {
+  int index = 0;
+  void next() {
+    setState(() {
+      if (index == 4) {
+        index = 0;
+      } else {
+        index += 1;
+      }
+    });
+  }
+
+  void before() {
+    setState(() {
+      if (index == 0) {
+        index = 4;
+      } else {
+        index -= 1;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[50],
+      appBar: AppBar(
+        backgroundColor: Colors.green[400],
+        title: const Text('Image viewer'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.navigate_before),
+            tooltip: 'Go to the previous image',
+            onPressed: before,
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 50, 0),
+            child: IconButton(
+              icon: const Icon(Icons.navigate_next),
+              tooltip: 'Go to the next image',
+              onPressed: next,
             ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 50, 0),
-              child: IconButton(
-                icon: const Icon(Icons.navigate_next),
-                tooltip: 'Go to the next image',
-                onPressed: () => {},
-              ),
-            ),
-          ],
-        ),
-        body: Image.asset(images[0]),
+          ),
+        ],
       ),
-    ));
+      body: Image.asset(images[index]),
+    );
+  }
+}
+
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false, // Why this line ? Can you explain it ?
+    //A: to eliminate teh debug banner that is usually on the right top side of the screen
+    home: ImageTurner(),
+  ),
+);
+
+// Q1: What type of state do you need to keep?   
+// A: the type of state that i need to keep or use is stateful as we need to update the picture so that it could changes when clicked. 
+
+// Q2 : Look at the current widget tree: which widget will you warp into your stateful widget?
+// A: that would be the Scaffold as we need the appbar as the one to set the state and the body for changing the image.
